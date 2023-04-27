1456af0b6d749f365601e5490d677ceb8edbf5dd
igoreduardobraga <igoredubraga@gmail.com>
2023-04-26 22:20:08 -0300

    Adicionando pasta de imagens

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -18,38 +18,37 @@ function deletar(dnum) {
   }
 }
 function mudarcor(num) {
-  const imgcv = 'Property 1=Variant2.png'
-  const imgc = 'Property 1=Default.png'
+  const imgcv = "img/Property 1=Variant2.png";
+  const imgc = "img/Property 1=Default.png";
   if (num === 1) {
     const meubotao = document.querySelector("#cr1");
     const imagen1 = meubotao.querySelector("#imagem1");
     
    
-    if (imagen1.getAttribute('src') === "Property 1=Default.png") {
-      
-      imagen1.setAttribute('src', imgcv);
+    if (imagen1.getAttribute("src") === "img/Property 1=Default.png") {
+      imagen1.setAttribute("src", imgcv);
     } else {
-      imagen1.setAttribute('src', imgc);
+      imagen1.setAttribute("src", imgc);
     }
   }
 
   if (num === 2) {
     const meubotao = document.querySelector("#cr2");
     const imagen1 = meubotao.querySelector("#imagem2");
-    if (imagen1.getAttribute('src') === "Property 1=Default.png") {
-      imagen1.setAttribute('src', imgcv);
+    if (imagen1.getAttribute("src") === "img/Property 1=Default.png") {
+      imagen1.setAttribute("src", imgcv);
     } else {
-      imagen1.setAttribute('src', imgc);
+      imagen1.setAttribute("src", imgc);
     }
   }
 
   if (num === 3) {
     const meubotao = document.querySelector("#cr3");
     const imagen1 = meubotao.querySelector("#imagem3");
-    if (imagen1.getAttribute('src') === "Property 1=Default.png") {
-      imagen1.setAttribute('src', imgcv);
+    if (imagen1.getAttribute("src") === "img/Property 1=Default.png") {
+      imagen1.setAttribute("src", imgcv);
     } else {
-      imagen1.setAttribute('src', imgc);
+      imagen1.setAttribute("src", imgc);
     }
   }
 }
@@ -84,18 +83,18 @@ function App() {
           <h1 className="ispot">
             iSpotify
             <img
-              src="icons8-registered-48.png"
+              src="img/icons8-registered-48.png"
               alt=""
               className="registrated"
             />
           </h1>
           <div className="artistas-musicas">
             <p className="alo3">
-              <img className="alo2" src="1378161.png"></img>
+              <img className="alo2" src="img/1378161.png"></img>
               <span className="oi">Artistas</span>
             </p>
             <p>
-            <img className="alo" src="Property 1=Default.png"></img>
+              <img className="alo" src="img/Property 1=Default.png"></img>
               <span className="oi">Músicas Curtidas</span>
             </p>
           </div>
@@ -110,7 +109,7 @@ function App() {
 
       <div className="main">
         <div className="upper">
-          <img className="album" src="\81TnWHafWdL._AC_SL1448_.jpg"></img>
+          <img className="album" src="img/\81TnWHafWdL._AC_SL1448_.jpg"></img>
           <div className="textos">
             <p className="playlist">Playlist</p>
             <p className="daily">Daily Mix 1</p>
@@ -120,11 +119,11 @@ function App() {
         </div>
 
         <div className="icons-top">
-          <img src="icons8-circled-play-65.png" alt="" className="play" />
-          <img src="Property 1=Default.png" alt="" className="heart" />
-          <img src="download-icon-down-arrow-23.png" id="download"></img>
-            
-          <img src="icons8-ellipsis-35.png" alt="" className="three-dots" />
+          <img src="img/icons8-circled-play-65.png" alt="" className="play" />
+          <img src="img/Property 1=Default.png" alt="" className="heart" />
+          <img src="img/download-icon-down-arrow-23.png" id="download"></img>
+
+          <img src="img/icons8-ellipsis-35.png" alt="" className="three-dots" />
         </div>
 
         <div class="cabeçalhoMusica">
@@ -142,7 +141,11 @@ function App() {
             </div>
             <span class="Album">By The Way</span>
             <button onClick={() => mudarcor(1)} class="coraçao" id="cr1">
-              <img  src="Property 1=Default.png" className="heart" id="imagem1"></img>
+              <img
+                src="img/Property 1=Default.png"
+                className="heart"
+                id="imagem1"
+              ></img>
             </button>
             <button onClick={() => deletar(1)} class="Relogio">
               <div className="re1">
@@ -160,7 +163,11 @@ function App() {
             </div>
             <span class="Album">X&Y</span>
             <button onClick={() => mudarcor(2)} class="coraçao" id="cr2">
-              <img src="Property 1=Default.png"  className="heart" id="imagem2"></img>
+              <img
+                src="img/Property 1=Default.png"
+                className="heart"
+                id="imagem2"
+              ></img>
             </button>
             <button onClick={() => deletar(2)} class="Relogio">
               <div className="re1">
@@ -178,7 +185,11 @@ function App() {
             </div>
             <span class="Album">Cidade Negra Acústico MTV</span>
             <button onClick={() => mudarcor(3)} class="coraçao" id="cr3">
-              <img src="Property 1=Default.png"  className="heart" id="imagem3"></img>
+              <img
+                src="img/Property 1=Default.png"
+                className="heart"
+                id="imagem3"
+              ></img>
             </button>
             <button onClick={() => deletar(3)} class="Relogio">
               <div className="re1">
