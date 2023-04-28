f1dec745446daeff4bec1a6f8d8111e5887b7351
João Silva <buzelin2@gmail.com>
2023-04-28 16:14:07 -0300

    Design dos artistas

--- my-app/src/routes/Artists.jsx
+++ my-app/src/routes/Artists.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import "./Artists.css";
 
 const Artists = () => {
   return (
-    <div></div>
+    <div className='artistas-main'>
+      <p className='text-top'>Artistas</p>
+      <div className='div1'>
+        <div className='artista art1'><p className='art-text'>Artista</p></div>
+        <div className='artista art2'><p className='art-text'>Artista</p></div>
+        <div className='artista art3'><p className='art-text'>Artista</p></div>
+        <div className='artista art4'><p className='art-text'>Artista</p></div>
+        <div className='artista art5'><p className='art-text'>Artista</p></div>
+      </div>
+      <div className='div2'>
+        <div className='artista art6'><p className='art-text'>Artista</p></div>
+        <div className='artista art7'><p className='art-text'>Artista</p></div>
+        <div className='artista art8'><p className='art-text'>Artista</p></div>
+        <div className='artista art9'><p className='art-text'>Artista</p></div>
+        <div className='artista art10'><p className='art-text'>Artista</p></div>
+      </div>
+    </div>
   )
 }
 
