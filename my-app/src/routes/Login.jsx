d07a0eb07713deca03f3fced9a037638bc4e3723
igoreduardobraga <igoredubraga@gmail.com>
2023-05-05 20:00:56 -0300

    Adicionando localStorage

--- my-app/src/routes/Login.jsx
+++ my-app/src/routes/Login.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
 import "./Login.css";
 
 const Login = () => {
-    const [color, setcolor] = useState();
-  
-    return (
-     <div>alo</div>
-    );
+  const [color, setcolor] = useState();
+
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
+  const [nome, setNome] = useState();
+  let dados = [];
+
+  const armazenarDados = () => {
+
+    let novoUsuario = {
+      email: email,
+      senha: senha,
+      nome: nome,
+    };
+
+    verificarExistencia();
+    dados.push(novoUsuario);
+    let dadosJSON = JSON.stringify(dados);
+    localStorage.setItem("dadosUsuarios", dadosJSON);
+  };
+
+  const verificarExistencia = () => {
+    let dadosExtraidos = localStorage.getItem("dadosUsuarios");
+    console.log('');
+    for(let i=0 ; i<dadosExtraidos.length ; i++){
+      console.log()
+      if(dadosExtraidos[i].email === email){
+        console.log("ESTE EMAIL JA ESTA CADASTRADO!!!!")
+      }
+    }
   }
-  
-  export default Login
+
+  return (
+    <div>
+      <h1>Inscrever-se em uma conta grátis do iSpotify ®</h1>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="text"
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)}
+        placeholder="Senha"
+      />
+      <input
+        type="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        placeholder="Nome"
+      />
+      <button
+        onClick={() => {
+          armazenarDados();
+        }}
+      >
+        Cadastrar
+      </button>
+    </div>
+  );
+};
+
+export default Login;
