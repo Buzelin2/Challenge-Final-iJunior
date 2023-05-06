3d4ca741bf328a49710aa92125c56404bbe8349b
luisotavio123 <113053832+luisotavio123@users.noreply.github.com>
2023-05-06 15:22:20 -0300

    Update Login.jsx
    terminando de criar as verificaçoes do localStorage

--- my-app/src/routes/Login.jsx
+++ my-app/src/routes/Login.jsx
@@ -3,52 +3,53 @@ import { useEffect, useState } from "react";
 import "./Login.css";
 
 const Login = () => {
-  const [color, setcolor] = useState();
+  
+  
 
   const [email, setEmail] = useState();
   const [senha, setSenha] = useState();
   const [nome, setNome] = useState();
-  let dados = [];
+  
 
   const armazenarDados = () => {
+      let aux = 0;
+      const email1 = document.querySelector('#email');
+      const senha1 = document.querySelector('#senha');
+      const emaill = email1.value;
+      const senhaa = senha1.value;
+     
+     for(var i = 0; i < localStorage.length; i++){
+      if(emaill ==  localStorage.key(i)){
+       aux++;
+      }
+     }
+     if(aux === 1){
+      alert("este email ja foi cadastrado")
+      
+     }else{
+      localStorage.setItem(emaill, senhaa);
+      
+     }
 
-    let novoUsuario = {
-      email: email,
-      senha: senha,
-      nome: nome,
-    };
-
-    verificarExistencia();
-    dados.push(novoUsuario);
-    let dadosJSON = JSON.stringify(dados);
-    localStorage.setItem("dadosUsuarios", dadosJSON);
   };
 
-  const verificarExistencia = () => {
-    let dadosExtraidos = localStorage.getItem("dadosUsuarios");
-    console.log('');
-    for(let i=0 ; i<dadosExtraidos.length ; i++){
-      console.log()
-      if(dadosExtraidos[i].email === email){
-        console.log("ESTE EMAIL JA ESTA CADASTRADO!!!!")
-      }
-    }
-  }
+  
 
   return (
     <div>
       <h1>Inscrever-se em uma conta grátis do iSpotify ®</h1>
       <input
         type="text"
-        value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Email"
+        id="email"
       />
       <input
         type="text"
         value={senha}
         onChange={(e) => setSenha(e.target.value)}
         placeholder="Senha"
+        id="senha"
       />
       <input
         type="text"
