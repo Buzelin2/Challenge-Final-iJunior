7d2396e818f3dc310e1d5d5007da6d7b5b9c2521
luisotavio123 <113053832+luisotavio123@users.noreply.github.com>
2023-05-07 10:06:53 -0300

    fazendo as verificações de email já existente

--- my-app/src/routes/Cadastro.jsx
+++ my-app/src/routes/Cadastro.jsx
@@ -1,41 +1,55 @@
-import React from 'react'
-import { useEffect, useState } from "react";
+import React, { useState } from 'react';
+
 import "./Cadastro.css";
+
 import LeftNavBar from "./components/LeftNavBar";
 
 const Cadastro = () => {
-    const [color, setcolor] = useState();
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
   
-    return (
-      <div>    
+  function verificar (){
+    
+    for (var i = 0; i < localStorage.length; i++) {
+      if (email === localStorage.key(i)){
+        const senha1 = localStorage.getItem(email);
+        if(senha1 === senha){
+          alert("logado");
+        } else {
+          alert("Esta senha está inválida");
+        }
+      }
+    }
+  }
+  
+  return (
+    <div>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-      <h1>iSpotify ®<br></br><br></br>
-        Música para todos</h1>
-      <div class="form-group">
+      <h1>iSpotify ®<br/><br/>Música para todos</h1>
+
+      <div className="form-group">
         <input
           type="text"
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
           id="email"
         />
-        <span class="material-symbols-outlined">mail</span>
+        <span className="material-symbols-outlined">mail</span>
       </div>
-      <div class="form-group">
+
+      <div className="form-group">
         <input
-          type="text"
+          type="password"
           value={senha}
           onChange={(e) => setSenha(e.target.value)}
           placeholder="Senha"
           id="senha"
         />
-        <span class="material-symbols-outlined">lock</span>
+        <span className="material-symbols-outlined">lock</span>
       </div>
-      <button class="Login"
-        onClick={() => {
-          armazenarDados();
-        }}
-      >
+
+      <button className="Login" onClick={() => { verificar(); }}>
         Entrar
       </button>
 
@@ -43,7 +57,7 @@ const Cadastro = () => {
         <p>Não tem uma conta? <a href="#">INSCREVA-SE</a></p>
       </div>
     </div>
-    );
-  }
-  
-  export default Cadastro
+  );
+}
+
+export default Cadastro;
