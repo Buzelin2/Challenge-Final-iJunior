7d0ec4518eb5edac9533b56d0a50113695b11a53
Gabrielzzz7 <gabrielcamposprudente19@gmail.com>
2023-05-07 22:03:05 -0300

    Ajuste na conexão das paginas e nos inputs

--- my-app/src/routes/Cadastro.jsx
+++ my-app/src/routes/Cadastro.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import "./Cadastro.css";
 
@@ -7,54 +8,98 @@ import LeftNavBar from "./components/LeftNavBar";
 const Cadastro = () => {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-  
-  function verificar (){
-    
-    for (var i = 0; i < localStorage.length; i++) {
-      if (email === localStorage.key(i)){
-        const senha1 = localStorage.getItem(email);
-        if(senha1 === senha){
-          alert("logado");
-        } else {
-          alert("Esta senha está inválida");
-        }
+
+  function validarCampos(campos) {
+    for (let i = 0; i < campos.length; i++) {
+      const campo = campos[i];
+      if (!campo.value) {
+        alert(`Por favor, preencha o campo ${campo.id}`);
+        return false;
       }
     }
+    return true;
   }
-  
+
+  function verificar() {
+    const campos = [
+      { id: 'email', value: email },
+      { id: 'senha', value: senha },
+    ];
+
+    if (!email && !senha) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
+
+    if (!validarCampos(campos)) {
+      return;
+    }
+
+    const storedSenha = localStorage.getItem(email);
+
+    if (storedSenha) {
+      if (storedSenha === senha) {
+        alert("Sucesso!");
+      } else {
+        alert("Esta senha está inválida");
+      }
+    } else {
+      alert("Email não cadastrado.");      
+    }
+
+    document.querySelector('#cadastroForm').reset();
+
+    // for (var i = 0; i < localStorage.length; i++) {
+    //   if (email === localStorage.key(i)) {
+    //     const senha1 = localStorage.getItem(email);
+    //     if (senha1 === senha) {
+    //       alert("logado");
+    //     } else {
+    //       alert("Esta senha está inválida");
+    //     }
+    //   }
+    // }
+  }
+
   return (
     <div>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-      <h1>iSpotify ®<br/><br/>Música para todos</h1>
+      <div class="principal">
+        <h1>iSpotify ®<br /><br />Música para todos</h1>
 
-      <div className="form-group">
-        <input
-          type="text"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          id="email"
-        />
-        <span className="material-symbols-outlined">mail</span>
-      </div>
+        <form class="cadastroForm">
+          <div className="form-group">
+            <input
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              id="email"
+              required
+            />
+            <span className="material-symbols-outlined">mail</span>
+          </div>
 
-      <div className="form-group">
-        <input
-          type="password"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-          placeholder="Senha"
-          id="senha"
-        />
-        <span className="material-symbols-outlined">lock</span>
-      </div>
+          <div className="form-group">
+            <input
+              type="password"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              placeholder="Senha"
+              id="senha"
+              required
+            />
+            <span className="material-symbols-outlined">lock</span>
+          </div>
 
-      <button className="Login" onClick={() => { verificar(); }}>
-        Entrar
-      </button>
+          <button className="Login" onClick={() => { verificar(); }}>
+            Entrar
+          </button>
+        </form>
 
-      <div className="fazerLogin">
-        <p>Não tem uma conta? <a href="#">INSCREVA-SE</a></p>
+        <div className="inscrever">
+          <p>Não tem uma conta? <Link to="/">INSCREVA-SE</Link></p>
+        </div>
       </div>
     </div>
   );
--- my-app/src/routes/Login.jsx
+++ my-app/src/routes/Login.jsx
@@ -1,40 +1,77 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 
 
 
 const Login = () => {
 
-
+  function validarCampos(campos) {
+    for (let i = 0; i < campos.length; i++) {
+      const campo = campos[i];
+      if (!campo.value) {
+        alert(`Por favor, preencha o campo ${campo.id}`);
+        return false;
+      }
+    }
+    return true;
+  }
 
   const [email, setEmail] = useState();
   const [senha, setSenha] = useState();
   const [nome, setNome] = useState();
 
 
-  const armazenarDados = () => {
 
-    let aux = 0;
+  const armazenarDados = () => {
     const email1 = document.querySelector('#email');
     const senha1 = document.querySelector('#senha');
     const emaill = email1.value;
     const senhaa = senha1.value;
 
-    for (var i = 0; i < localStorage.length; i++) {
-      if (emaill == localStorage.key(i)) {
-        aux++;
-      }
-    }
-    if (aux === 1) {
-      alert("este email ja foi cadastrado")
+    const campos = [
+      { id: 'email', value: email },
+      { id: 'senha', value: senha },
+      { id: 'nome', value: nome },
+    ];
 
+    if (!nome && !email && !senha) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
+
+    if (!validarCampos(campos)) {
+      return;
+    }
+
+    if (localStorage.getItem(emaill)) {
+      alert("Este email já foi cadastrado.");
     } else {
       localStorage.setItem(emaill, senhaa);
-
+      alert("Cadastro realizado com sucesso!");
+      document.querySelector('#cadastroForm').reset();
     }
 
+    // let aux = 0;
+    // const email1 = document.querySelector('#email');
+    // const senha1 = document.querySelector('#senha');
+    // const emaill = email1.value;
+    // const senhaa = senha1.value;
+
+    // for (var i = 0; i < localStorage.length; i++) {
+    //   if (emaill == localStorage.key(i)) {
+    //     aux++;
+    //   }
+    // }
+    // if (aux === 1) {
+    //   alert("este email ja foi cadastrado")
+
+    // } else {
+    //   localStorage.setItem(emaill, senhaa);
+
+    // }
   };
 
 
@@ -48,47 +85,55 @@ const Login = () => {
       <h1>Inscrever-se em uma <br></br>
         conta grátis do <br></br>
         iSpotify ®</h1>
-      <div class="form-group">
-        <input
-          type="text"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          id="email"
-        />
-        <span class="material-symbols-outlined">mail</span>
-        
-      </div>
-      <div class="form-group">
-        <input
-          type="text"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-          placeholder="Crie uma Senha"
-          id="senha"
-        />
-        <span class="material-symbols-outlined">lock</span>
-      </div>
-      <div class="form-group">
-        <input
-          type="text"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          placeholder="Como devemos chamar você?"
-        />
-        <span class="material-symbols-outlined">
-          account_circle
-        </span>
-      </div>
-      <button class="cadastrar"
-        onClick={() => {
-          armazenarDados();
-        }}
-      >
-        Cadastrar
-      </button>
+
+      <form id="cadastroForm">
+        <div class="form-group">
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            id="email"
+            required
+          />
+          <span class="material-symbols-outlined">mail</span>
+
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder="Crie uma Senha"
+            id="senha"
+            required
+          />
+          <span class="material-symbols-outlined">lock</span>
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Como devemos chamar você?"
+            id="nome"
+            required
+          />
+          <span class="material-symbols-outlined">
+            account_circle
+          </span>
+        </div>
+        <button class="cadastrar"
+          onClick={() => {
+            armazenarDados();
+          }}
+        >
+          Cadastrar
+        </button>
+      </form>
 
       <div className="fazerLogin">
-        <p>Já é um usuário do iSpotify? <a href="#">FAÇA LOGIN</a></p>
+        <p>Já é um usuário do iSpotify? <Link to="./cadastro">FAÇA LOGIN</Link></p>
       </div>
     </div>
   );
