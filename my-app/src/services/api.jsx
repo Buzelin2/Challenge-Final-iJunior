2da212bfdcd58659b13814a65316c9a53071e8dd
igoreduardobraga <igoredubraga@gmail.com>
2023-05-17 15:36:36 -0300

    adicionando link nos nomes dos artistas

--- my-app/src/services/api.jsx
+++ my-app/src/services/api.jsx
@@ -5,7 +5,8 @@ const api = axios.create({
   withCredentials: true,
 });
 
-const token ='BQAkBSMiPDTGI3lqaqAabNWrxHfVM82KS1usm4NXN0h8gktRTAxhCxqdNsN-hYdK0GWGMeoj0y6H4rDgsbGV-O-1_f0h6o5C2mtXRm846Ic2A0JotCgoZYbzY4K7ky-lzSuJfx--avhum0MEMFH0KTu-ZVVODyOfOmOr5z2ixtA09-6HIGlTeR2T_1JIwDFwRn5daHYuElQBUlOQ3sZBXtNWYL5UOHCsG5oKs8C4_kYEb47Qo44r59ZMy9ZDHCiD4hNaxxOoPxl2_Gc2Oiy8cPwI_Q';
+const token =
+  "BQAvbrJMEl-7noizkB2Z8L_Gr2PUS2i9fQT9CSpE_Z42_0OJoPaEtspoKRylKLC73ziJZcqYomGEuETg824cIipSfw0CuHmf5WQuyJ20rBVzz2doecDaopN9e3DZtFeeOFV9MtT2zY_hOOL0k6GsFJqfi6kodAwv4YacuHL_Cn5USONNaZ9SD5ZbOn17I9AsrLjIf_b3kTzF6m0tfCtHPgyw_OWsbyOAb1EOwuIWkjho_ZuyE50MLmmUXJL6rQ2eqxsuBhA";
 const apiSpotify = axios.create({
   baseURL: "https://api.spotify.com/v1/",
   headers: {
